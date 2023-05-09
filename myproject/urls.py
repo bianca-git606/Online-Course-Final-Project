ec108101c82557b0705262e2c32f72909478385f
biancagit-606 <biancafiedalan31@yahoo.com>
2023-05-09 13:19:31 +1200

    made migrations and initialized database

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from onlinecourse import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('onlinecourse/', include('onlinecourse.urls')),
+    path(route='', view=views.CourseListView.as_view(), name='popular_course_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
